f727496e85cfc93aea2d5e027bb2edc1343b1aa2
Patri <patrilagoespino@gmail.com>
2026-05-28 11:58:24 +0200

    Cambio cv

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import './Hero.css'
 
 import iconGithub from "./../../assets/iconos/iconGithub.png"
 import iconLinkedin from "./../../assets/iconos/iconLinkedin.png"
-import patriCv from "./../../assets/cv/PatriciaLago_CV.pdf"
+import patriCv from "./../../assets/cv/PatriciaLago_FullStack_CV.pdf"
 
 const Hero = () => {
 
